1aa09103d0eb82cb0fa685cf0a42d46a66e942cf
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2023-09-21 14:09:26 -0600

    finis

--- main.js
+++ main.js
@@ -25,38 +25,71 @@ function btnClicked() {
   } else if (selection === "edit") {
     edit();
   }
+
+  // Output Array as a List
+  outputEl.innerHTML = ``;
+
+  for (let i = 0; i < arrayList.length; i++) {
+    outputEl.innerHTML += `<div>${i + 1}: ${arrayList[i]}</div>`;
+  }
+
+  if (arrayList.length <= 0) {
+    outputEl.innerHTML = `<div>Item Placeholder</div>`
+  }
 }
 
 // Menu Option Functions
 function addItem() {
   console.log("Add Item");
   
-  let addNewItem = prompt("Enter Item:");
-  arrayList.push(addNewItem);
-
-  outputEl.innerHTML = ``;
-
-  for (let i = 0; i < arrayList.length; i++) {
-    outputEl.innerHTML += `<div>${i + 1}: ${arrayList[i]}</div>`;
-  }
+  let newItem = prompt("Enter Item:");
+  arrayList.push(newItem);
 }
 
 function removeLast() {
   console.log("Remove Last");
+
+  arrayList.pop();
 }
 
 function insert() {
   console.log("Insert");
+
+  let position = +prompt("Insert Position:");
+  let newItem = prompt("Item to Insert:");
+
+  arrayList.splice(position - 1, 0, newItem);
 }
 
 function removePos() {
   console.log("Remove at Position");
+
+  let position = +prompt("Position to Remove:");
+
+  arrayList.splice(position - 1, 1);
 }
 
 function move() {
   console.log("Move");
+
+  let position1 = +prompt("Move item from:");
+  let position2 = +prompt("Move item to:");
+
+  let item1 = arrayList[position1];
+  let item2 = arrayList[position2];
+
+  arrayList.splice(position2 - 1, 0, item1);
+  arrayList.splice(position2, 1);
+  arrayList.splice(position1 - 1, 0, item2);
+  arrayList.splice(position1, 1);
 }
 
 function edit() {
   console.log("Edit");
+
+  let position = +prompt("Enter Position:");
+  let newItem = prompt("Replace with:");
+
+  arrayList.splice(position - 1, 0, newItem);
+  arrayList.splice(position, 1);
 }
